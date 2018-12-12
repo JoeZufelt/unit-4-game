80e1f69551d2deecaf083b40b651378272b2ae7e
Joe Zufelt <joe.zufelt@gmail.com>
2018-12-12 18:02:53 -0500

    so close

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,15 @@ $(document).ready(function(){
 var wins = 0;
 var losses = 0;
 var compNumber = Math.floor(Math.random() * 100 + 50);
-var yourScore;
+var yourScore = 0;
+var newScore = 0;
 var rupeeGreen = Math.floor(Math.random() * 20 + 2);
 var rupeeBlue = Math.floor(Math.random() * 20 + 2);
 var rupeeRed = Math.floor(Math.random() * 20 + 2);
 var rupeePurple = Math.floor(Math.random() * 20 + 2);
 
 // Assign Rupees/comp a Number
+function newNumbers() {
 $("#rupeeGreen").val(rupeeGreen);
 $("#rupeeBlue").val(rupeeBlue);
 $("#rupeeRed").val(rupeeRed);
@@ -21,34 +23,55 @@ console.log(rupeeBlue);
 console.log(rupeeRed);
 console.log(rupeePurple);
 console.log(compNumber);
+};
+
+function newGame () {
+    newNumbers();
+    newScore = 0;
+    $("#randNum").text(compNumber);
+    $("#score").text(yourScore);
+    $("#rupeeGreen").attr("rupeeGreen", rupeeGreen);
+    $("#rupeeBlue").attr("data-crystalvalue", rupeeBlue);
+    $("#rupeeRed").attr("data-crystalvalue", rupeeRed);
+    $("#rupeePurple").attr("data-crystalvalue", rupeePurple);
+    $("wins").text(wins);
+    $("#losses").text(losses);
+}
+newNumbers();
+newGame();
 
 // Put compNum into HTML
 $("#randNum").html(compNumber);
 
 // Assign Rupee Clicks to Score
 $("#rupeeGreen").on("click", function(){
-    $("#score").html(rupeeGreen);
+    yourScore = yourScore + rupeeGreen;
+    $("#score").html(yourScore);
 });
 $("#rupeeBlue").on("click", function(){
-    $("#score").html(rupeeBlue);
+    yourScore = yourScore + rupeeBlue;
+    $("#score").html(yourScore);
 });
 $("#rupeeRed").on("click", function(){
-    $("#score").html(rupeeRed);
+    yourScore = yourScore + rupeeRed;
+    $("#score").html(yourScore);
 });
 $("#rupeePurple").on("click", function(){
-    $("#score").html(rupeePurple);
+    yourScore = yourScore + rupeePurple;
+    $("#score").html(yourScore);
 });
-
-// Add up your score
 
 
 // if/else for wins/losses
-if (yourScore === compNumber) {
-    $("#wins").html(wins++);
-} else if (yourScore < compNumber) {
 
+if (yourScore == compNumber) {
+    $("#wins").html("Wins: " + wins++);
+    newGame();
+} else if (yourScore > compNumber) {
+    $("#losses").html("Losses: " + losses++);
+    newGame();
 } else {
-    $("#losses").html(losses++);
+
 };
 
 }); // End of JQuery
