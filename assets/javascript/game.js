dd68044c3d64ce36f0e7f04f6e9bc361f9769443
Joe Zufelt <joe.zufelt@gmail.com>
2018-12-13 18:20:55 -0500

    DONE

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,125 @@
-$(document).ready(function(){
-// Variables
+$(document).ready(function () {
+    // Variables
 
-var wins = 0;
-var losses = 0;
-var compNumber = Math.floor(Math.random() * 100 + 50);
-var yourScore = 0;
-var newScore = 0;
-var rupeeGreen = Math.floor(Math.random() * 20 + 2);
-var rupeeBlue = Math.floor(Math.random() * 20 + 2);
-var rupeeRed = Math.floor(Math.random() * 20 + 2);
-var rupeePurple = Math.floor(Math.random() * 20 + 2);
+    var wins = 0;
+    var losses = 0;
+    var compNumber = Math.floor(Math.random() * 100 + 50);
+    var yourScore = 0;
+    var rupeeGreen = Math.floor(Math.random() * 20 + 2);
+    var rupeeBlue = Math.floor(Math.random() * 20 + 2);
+    var rupeeRed = Math.floor(Math.random() * 20 + 2);
+    var rupeePurple = Math.floor(Math.random() * 20 + 2);
 
-// Assign Rupees/comp a Number
-function newNumbers() {
-$("#rupeeGreen").val(rupeeGreen);
-$("#rupeeBlue").val(rupeeBlue);
-$("#rupeeRed").val(rupeeRed);
-$("#rupeePruple").val(rupeePurple);
-$("#randNum").val(compNumber);
-console.log(rupeeGreen);
-console.log(rupeeBlue);
-console.log(rupeeRed);
-console.log(rupeePurple);
-console.log(compNumber);
-};
-
-function newGame () {
+    // Assign Rupees/comp a Number
+    function newNumbers() {
+        rupeeGreen = Math.floor(Math.random() * 20 + 2);
+        rupeeBlue = Math.floor(Math.random() * 20 + 2);
+        rupeeRed = Math.floor(Math.random() * 20 + 2);
+        rupeePurple = Math.floor(Math.random() * 20 + 2);
+        compNumber = Math.floor(Math.random() * 100 + 50);
+        $("#rupeeGreen").val(rupeeGreen);
+        $("#rupeeBlue").val(rupeeBlue);
+        $("#rupeeRed").val(rupeeRed);
+        $("#rupeePruple").val(rupeePurple);
+        $("#randNum").val(compNumber);
+        console.log(rupeeGreen);
+        console.log(rupeeBlue);
+        console.log(rupeeRed);
+        console.log(rupeePurple);
+        console.log(compNumber);
+    };
     newNumbers();
-    newScore = 0;
+
+    function newGame() {
+        newNumbers();
+        yourScore = 0;
+        $("#randNum").text(compNumber);
+        $("#score").text(yourScore);
+        // $("#rupeeGreen").attr(rupeeGreen);
+        // $("#rupeeBlue").attr(rupeeBlue);
+        // $("#rupeeRed").attr(rupeeRed);
+        // $("#rupeePurple").attr(rupeePurple);
+        // $("wins").text(wins);
+        // $("#losses").text(losses);
+    }
+
+    // Put compNum into HTML
     $("#randNum").text(compNumber);
-    $("#score").text(yourScore);
-    $("#rupeeGreen").attr("rupeeGreen", rupeeGreen);
-    $("#rupeeBlue").attr("data-crystalvalue", rupeeBlue);
-    $("#rupeeRed").attr("data-crystalvalue", rupeeRed);
-    $("#rupeePurple").attr("data-crystalvalue", rupeePurple);
-    $("wins").text(wins);
-    $("#losses").text(losses);
-}
-newNumbers();
-newGame();
 
-// Put compNum into HTML
-$("#randNum").html(compNumber);
+    // Assign Rupee Clicks to Score
+    $("#rupeeGreen").on("click", function () {
+        yourScore = yourScore + rupeeGreen;
+        $("#score").text(yourScore);
+        if (yourScore === compNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            newGame();
+        } else if (yourScore > compNumber) {
+            losses++
+            $("#losses").text("Losses: " + losses);
+            newGame();
+        } else {
 
-// Assign Rupee Clicks to Score
-$("#rupeeGreen").on("click", function(){
-    yourScore = yourScore + rupeeGreen;
-    $("#score").html(yourScore);
-});
-$("#rupeeBlue").on("click", function(){
-    yourScore = yourScore + rupeeBlue;
-    $("#score").html(yourScore);
-});
-$("#rupeeRed").on("click", function(){
-    yourScore = yourScore + rupeeRed;
-    $("#score").html(yourScore);
-});
-$("#rupeePurple").on("click", function(){
-    yourScore = yourScore + rupeePurple;
-    $("#score").html(yourScore);
-});
+        };
+    });
+    $("#rupeeBlue").on("click", function () {
+        yourScore = yourScore + rupeeBlue;
+        $("#score").text(yourScore);
+        if (yourScore === compNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            newGame();
+        } else if (yourScore > compNumber) {
+            losses++;
+            $("#losses").text("Losses: "+ losses);
+            newGame();
+        } else {
+
+        };
+    });
+    $("#rupeeRed").on("click", function () {
+        yourScore = yourScore + rupeeRed;
+        $("#score").text(yourScore);
+        if (yourScore === compNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            newGame();
+        } else if (yourScore > compNumber) {
+            losses++;
+            $("#losses").text("Losses: " + losses);
+            newGame();
+        } else {
+
+        };
+    });
+    $("#rupeePurple").on("click", function () {
+        yourScore = yourScore + rupeePurple;
+        $("#score").text(yourScore);
+        if (yourScore === compNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            newGame();
+        } else if (yourScore > compNumber) {
+            losses++
+            $("#losses").text("Losses: " + losses);
+            newGame();
+        } else {
+
+        };
+    });
 
 
-// if/else for wins/losses
+    // // if/else for wins/losses
+    // function endGame() {
+    // if (yourScore === compNumber) {
+    //     $("#wins").html("Wins: " + wins++);
+    //     newGame();
+    // } else if (yourScore > compNumber) {
+    //     $("#losses").html("Losses: " + losses++);
+    //     newGame();
+    // } else {
 
-if (yourScore == compNumber) {
-    $("#wins").html("Wins: " + wins++);
-    newGame();
-} else if (yourScore > compNumber) {
-    $("#losses").html("Losses: " + losses++);
-    newGame();
-} else {
-
-};
+    // };
+    // };
 
 }); // End of JQuery
